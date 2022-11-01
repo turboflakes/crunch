22ca6b122019237c58a1ca51c0180b7c1998445e
paulo <paulo.rmartins@gmail.com>
2022-11-01 13:30:58 +0000

    fix other chains

--- src/crunch.rs
+++ src/crunch.rs
@@ -22,7 +22,7 @@ use crate::config::{Config, CONFIG};
 use crate::errors::CrunchError;
 use crate::matrix::Matrix;
 use crate::runtimes::{
-    kusama, polkadot,
+    aleph_zero, aleph_zero_testnet, kusama, polkadot, lagoon,
     support::{ChainPrefix, ChainTokenSymbol, SupportedRuntime},
     westend,
 };
@@ -199,10 +199,10 @@ impl Crunch {
             SupportedRuntime::Polkadot => polkadot::inspect(self).await,
             SupportedRuntime::Kusama => kusama::inspect(self).await,
             SupportedRuntime::Westend => westend::inspect(self).await,
-            // SupportedRuntime::AlephZero => aleph_zero::inspect(self).await,
-            // SupportedRuntime::AlephZeroTestnet => aleph_zero_testnet::inspect(self).await,
-            // SupportedRuntime::Lagoon => lagoon::inspect(self).await,
-            _ => unreachable!(),
+            SupportedRuntime::AlephZero => aleph_zero::inspect(self).await,
+            SupportedRuntime::AlephZeroTestnet => aleph_zero_testnet::inspect(self).await,
+            SupportedRuntime::Lagoon => lagoon::inspect(self).await,
+            // _ => unreachable!(),
         }
     }
 
@@ -211,12 +211,12 @@ impl Crunch {
             SupportedRuntime::Polkadot => polkadot::try_run_batch(self, None).await,
             SupportedRuntime::Kusama => kusama::try_run_batch(self, None).await,
             SupportedRuntime::Westend => westend::try_run_batch(self, None).await,
-            // SupportedRuntime::AlephZero => aleph_zero::try_run_batch(self, None).await,
-            // SupportedRuntime::AlephZeroTestnet => {
-            //     aleph_zero_testnet::try_run_batch(self, None).await
-            // }
-            // SupportedRuntime::Lagoon => lagoon::try_run_batch(self, None).await,
-            _ => unreachable!(),
+            SupportedRuntime::AlephZero => aleph_zero::try_run_batch(self, None).await,
+            SupportedRuntime::AlephZeroTestnet => {
+                aleph_zero_testnet::try_run_batch(self, None).await
+            }
+            SupportedRuntime::Lagoon => lagoon::try_run_batch(self, None).await,
+            // _ => unreachable!(),
         }
     }
 
@@ -231,16 +231,16 @@ impl Crunch {
             SupportedRuntime::Westend => {
                 westend::run_and_subscribe_era_paid_events(self).await
             }
-            // SupportedRuntime::AlephZero => {
-            //     aleph_zero::run_and_subscribe_era_paid_events(self).await
-            // }
-            // SupportedRuntime::AlephZeroTestnet => {
-            //     aleph_zero_testnet::run_and_subscribe_era_paid_events(self).await
-            // }
-            // SupportedRuntime::Lagoon => {
-            //     lagoon::run_and_subscribe_era_paid_events(self).await
-            // }
-            _ => unreachable!(),
+            SupportedRuntime::AlephZero => {
+                aleph_zero::run_and_subscribe_era_paid_events(self).await
+            }
+            SupportedRuntime::AlephZeroTestnet => {
+                aleph_zero_testnet::run_and_subscribe_era_paid_events(self).await
+            }
+            SupportedRuntime::Lagoon => {
+                lagoon::run_and_subscribe_era_paid_events(self).await
+            }
+            // _ => unreachable!(),
         }
     }
 }
