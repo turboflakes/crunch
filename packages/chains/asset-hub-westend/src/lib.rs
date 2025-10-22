1e48e00f1954c119a7d4b27aca306d177ad506d1
Paulo Martins <paulormart@users.noreply.github.com>
2025-10-22 11:58:53 +0100

    clean unused functions

--- packages/chains/asset-hub-westend/src/lib.rs
+++ packages/chains/asset-hub-westend/src/lib.rs
@@ -22,18 +22,16 @@
 use async_recursion::async_recursion;
 use crunch_config::CONFIG;
 use crunch_core::{
-    get_account_id_from_storage_key, get_keypair_from_seed_file, random_wait, to_hex,
-    try_fetch_onet_data, try_fetch_stashes_from_remote_url, Crunch, NominatorsAmount,
-    ValidatorAmount, ValidatorIndex,
+    get_account_id_from_storage_key, to_hex, try_fetch_stashes_from_remote_url, Crunch,
+    NominatorsAmount, ValidatorAmount, ValidatorIndex,
 };
 use crunch_error::CrunchError;
 use crunch_people_westend::get_display_name;
 use crunch_pools::{nomination_pool_account, AccountType};
 use crunch_relay_chain_westend::rc_metadata;
 use crunch_report::{
-    replace_emoji_lowercase, Batch, EraIndex, Network, NominationPoolCommission,
-    NominationPoolsSummary, Payout, PayoutSummary, Points, RawData, Report,
-    SignerDetails, Validator, Validators,
+    replace_emoji_lowercase, Batch, EraIndex, NominationPoolCommission,
+    NominationPoolsSummary, Payout, PayoutSummary, Points, Validator, Validators,
 };
 use log::{debug, info, warn};
 use std::{
@@ -41,7 +39,6 @@ use std::{
     convert::{TryFrom, TryInto},
     result::Result,
     str::FromStr,
-    thread, time,
 };
 use subxt::{
     config::polkadot::PolkadotExtrinsicParamsBuilder as TxParams,
@@ -69,7 +66,7 @@ use ah_metadata::{
         pallet_nomination_pools::{BondExtra, ClaimPermission},
         xcm_runtime_apis::dry_run::CallDryRunEffects,
     },
-    staking::events::{EraPaid, PayoutStarted, Rewarded},
+    staking::events::{PayoutStarted, Rewarded},
     system::events::ExtrinsicFailed,
     utility::{
         calls::types::with_weight::Weight,
@@ -84,267 +81,6 @@ type Call = ah_metadata::runtime_types::asset_hub_westend_runtime::RuntimeCall;
 type StakingCall = ah_metadata::runtime_types::pallet_staking_async::pallet::pallet::Call;
 type NominationPoolsCall =
     ah_metadata::runtime_types::pallet_nomination_pools::pallet::Call;
-
-pub async fn run_and_subscribe_era_paid_events(
-    crunch: &Crunch,
-) -> Result<(), CrunchError> {
-    info!("Inspect and `crunch` unclaimed payout rewards");
-    // Run once before start subscription
-    try_crunch(&crunch).await?;
-    let mut latest_block_number_processed: Option<u32> = Some(0);
-    info!("Subscribe 'EraPaid' on-chain finalized event");
-    let api = crunch
-        .asset_hub_client()
-        .as_ref()
-        .expect("AH API to be available");
-    let mut block_sub = api.blocks().subscribe_finalized().await?;
-    while let Some(block) = block_sub.next().await {
-        // let block = block?;
-
-        // Silently handle RPC disconnection and wait for the next block as soon as reconnection is available
-        let block = match block {
-            Ok(b) => b,
-            Err(e) => {
-                if e.is_disconnected_will_reconnect() {
-                    warn!("The RPC connection was dropped will try to reconnect.");
-                    continue;
-                }
-                return Err(e.into());
-            }
-        };
-
-        // Process blocks that might have been dropped while reconnecting
-        while let Some(processed_block_number) = latest_block_number_processed {
-            if block.number() == processed_block_number || processed_block_number == 0 {
-                latest_block_number_processed = None;
-            } else {
-                let block_number = processed_block_number + 1;
-
-                // Skip current block and fetch only blocks that have not yet been processed
-                if block.number() - block_number > 0 {
-                    if let Some(block_hash) = crunch
-                        .rpc()
-                        .chain_get_block_hash(Some(block_number.into()))
-                        .await?
-                    {
-                        let events = api.events().at(block_hash).await?;
-
-                        // Event --> staking::EraPaid
-                        if let Some(_event) = events.find_first::<EraPaid>()? {
-                            let wait: u64 = random_wait(240);
-                            info!("Waiting {} seconds before run batch", wait);
-                            thread::sleep(time::Duration::from_secs(wait));
-                            try_crunch(&crunch).await?;
-                        }
-                    }
-                }
-
-                latest_block_number_processed = Some(block_number);
-            }
-        }
-
-        let events = block.events().await?;
-
-        // Event --> staking::EraPaid
-        if let Some(_event) = events.find_first::<EraPaid>()? {
-            let wait: u64 = random_wait(240);
-            info!("Waiting {} seconds before run batch", wait);
-            thread::sleep(time::Duration::from_secs(wait));
-            try_crunch(&crunch).await?;
-        }
-
-        latest_block_number_processed = Some(block.number());
-    }
-    // If subscription has closed for some reason await and subscribe again
-    Err(CrunchError::SubscriptionFinished)
-}
-
-pub async fn try_crunch(crunch: &Crunch) -> Result<(), CrunchError> {
-    let config = CONFIG.clone();
-    let api = crunch
-        .asset_hub_client()
-        .as_ref()
-        .expect("AH API to be available");
-
-    let signer_keypair: Keypair = get_keypair_from_seed_file()?;
-    let seed_account_id: AccountId32 = signer_keypair.public_key().into();
-
-    // Get signer account identity
-    let (signer_name, _, _) = get_display_name(&crunch, &seed_account_id, None).await?;
-    let mut signer_details = SignerDetails {
-        account: seed_account_id.clone(),
-        name: signer_name,
-        warnings: Vec::new(),
-    };
-    info!("signer_details {:?}", signer_details);
-
-    // Warn if signer account is running low on funds (if lower than 2x Existential Deposit)
-    let ed_addr = ah_metadata::constants().balances().existential_deposit();
-    let ed = api.constants().at(&ed_addr)?;
-
-    let seed_account_info_addr = ah_metadata::storage()
-        .system()
-        .account(seed_account_id.clone());
-    if let Some(seed_account_info) = api
-        .storage()
-        .at_latest()
-        .await?
-        .fetch(&seed_account_info_addr)
-        .await?
-    {
-        if seed_account_info.data.free
-            <= (config.existential_deposit_factor_warning as u128 * ed)
-        {
-            let warning = "⚡ Signer account is running low on funds ⚡";
-            signer_details.warnings.push(warning.to_string());
-            warn!("{warning}");
-        }
-    } else {
-        let chain_name = crunch.rpc().system_chain().await?;
-        warn!("Signer account {seed_account_id} not found on the {chain_name} network!");
-    }
-
-    // Get Network name
-    let chain_name = crunch.rpc().system_chain().await?;
-
-    // Get Era index
-    let active_era_addr = ah_metadata::storage().staking().active_era();
-    let active_era_index = match api
-        .storage()
-        .at_latest()
-        .await?
-        .fetch(&active_era_addr)
-        .await?
-    {
-        Some(info) => info.index,
-        None => return Err(CrunchError::Other("Active era not available".into())),
-    };
-
-    let properties = crunch.rpc().system_properties().await?;
-
-    // Get Token symbol
-    let token_symbol: String = if let Some(token_symbol) = properties.get("tokenSymbol") {
-        token_symbol.as_str().unwrap_or_default().to_string()
-    } else {
-        "ND".to_string()
-    };
-
-    // Get Token decimals
-    let token_decimals: u8 = if let Some(token_decimals) = properties.get("tokenDecimals")
-    {
-        token_decimals
-            .as_u64()
-            .unwrap_or_default()
-            .try_into()
-            .unwrap()
-    } else {
-        12
-    };
-
-    let subdomain = crunch.subdomain(true);
-
-    // Set network info
-    let network = Network {
-        name: chain_name.clone(),
-        subdomain,
-        active_era: active_era_index,
-        token_symbol,
-        token_decimals,
-    };
-    debug!("network {:?}", network);
-
-    // Check if group by identity is enabled by user to change the behaviour of how stashes are processed
-    if config.group_identity_enabled {
-        // Try run payouts in batches
-        let mut all_validators =
-            collect_validators_data(&crunch, active_era_index).await?;
-
-        let parent_identities: Vec<String> =
-            get_distinct_parent_identites(all_validators.clone());
-
-        for parent in parent_identities {
-            // Filter validators by parent identity
-            let mut validators = all_validators
-                .clone()
-                .into_iter()
-                .filter(|v| replace_emoji_lowercase(&v.parent_identity) == parent)
-                .collect::<Validators>();
-
-            // Remove all processed validators from original vec so it don't get looked up again
-            all_validators
-                .retain(|v| replace_emoji_lowercase(&v.parent_identity) != parent);
-
-            if validators.len() > 0 {
-                // Try run payouts in batches
-                let payout_summary =
-                    try_run_batch_payouts(&crunch, &signer_keypair, &mut validators)
-                        .await?;
-
-                // Try fetch ONE-T grade data
-                for v in &mut validators {
-                    v.onet =
-                        try_fetch_onet_data(chain_name.to_lowercase(), v.stash.clone())
-                            .await?;
-                }
-
-                // NOTE: In the last iteration try to batch pools if any and include them in the report
-                // TODO: Eventually we could do a separate message containing only the pools report
-                let pools_summary: Option<NominationPoolsSummary> =
-                    if all_validators.len() == 0 {
-                        // Try run pool members in batches
-                        Some(try_run_batch_pool_members(&crunch, &signer_keypair).await?)
-                    } else {
-                        None
-                    };
-
-                let data = RawData {
-                    network: network.clone(),
-                    signer_details: signer_details.clone(),
-                    validators,
-                    payout_summary,
-                    pools_summary,
-                };
-
-                let report = Report::from(data);
-                crunch
-                    .send_message(&report.message(), &report.formatted_message())
-                    .await?;
-            }
-            // NOTE: To prevent too many request from matrix API set a sleep here of 5 seconds before trying another identity payout
-            thread::sleep(time::Duration::from_secs(5));
-        }
-    } else {
-        let mut validators = collect_validators_data(&crunch, active_era_index).await?;
-
-        // Try run payouts in batches
-        let payout_summary =
-            try_run_batch_payouts(&crunch, &signer_keypair, &mut validators).await?;
-
-        // Try fetch ONE-T grade data
-        for v in &mut validators {
-            v.onet =
-                try_fetch_onet_data(chain_name.to_lowercase(), v.stash.clone()).await?;
-        }
-
-        // Try run members in batches
-        let pools_summary = try_run_batch_pool_members(&crunch, &signer_keypair).await?;
-
-        let data = RawData {
-            network,
-            signer_details,
-            validators,
-            payout_summary,
-            pools_summary: Some(pools_summary),
-        };
-
-        let report = Report::from(data);
-        crunch
-            .send_message(&report.message(), &report.formatted_message())
-            .await?;
-    }
-
-    Ok(())
-}
 
 pub async fn try_run_batch_pool_members(
     crunch: &Crunch,
@@ -942,18 +678,23 @@ pub async fn validate_calls_for_batch(
         Ok(_) => {
             if let Some(ref pending) = pending_calls {
                 info!(
-                    "Batch call validated successfully, pending calls: {:?}",
+                    "Batch validated with {} calls successfully. Pending calls: {}",
+                    calls.len(),
                     pending.len()
                 );
             } else {
-                info!("Batch call validated: {:?}", batch_call);
+                info!("Batch validated with {} calls successfully", calls.len());
             }
 
             return Ok((calls.clone(), pending_calls));
         }
         Err(err) => match err {
             CrunchError::MaxWeightExceeded(e) => {
-                warn!("Max weight exceeded: {}", e);
+                warn!(
+                    "Batch with {} calls got weight exceeded: {}. Trying with one less call.",
+                    calls.len(),
+                    e
+                );
                 if calls.len() > 1 {
                     let new_calls = calls[..calls.len() - 1].to_vec();
                     let last_call = calls[calls.len() - 1..].to_vec();
